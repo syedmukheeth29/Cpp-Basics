5e051d5a8c5564d25b3f0c2fd74a68efe686c8ac
Syed Abdul Mukheeth Peer <syedmukheeth09@gmail.com>
2025-05-14 18:51:34 +0530

    constant can't be updated

--- problems/2.cpp
+++ problems/2.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
 using namespace std;
-
-int main() {
-    
+// 2.	Declare a const integer and try modifying it. What happens?
+int main()
+{
+    const int myluckynois = 29;
+    cout << "The lucky number is: " << myluckynois << endl;
+    // int anotherLuckyNois=5;
+    int myluckynois = 5;
+    // you get an error because you are trying to modify a const variable
+    // const does not modified by just updating the no, devil trick "use pointer"(in future i will use)
+    cout << "Another lucky number is: " << myluckynois << endl;
     return 0;
 }
